73de7675e31054fb5dd0862ad123366fb019fd33
JonatSrG <JonatSrG>
2022-09-26 00:34:24 -0500

    fetch-5-1

--- fetch-5.js
+++ fetch-5.js
@@ -1,18 +1,18 @@
 
-fetch('https://reqres.in/api/users/1')
+fetch('https://reqres.in/api/users/1000')
     .then(resp => {
 
-        resp.clone().json().then( usuario => {
-            console.log(usuario.data);
-        });
+        if ( resp.ok ) {
 
-        resp.clone().json().then( usuario => {
-            console.log(usuario.data);
-        });
+            return resp.json();
 
-        resp.json().then( usuario => {
-            console.log(usuario.data);
-        });
+        }else {
+            throw new Error('No existe el usuario 1000')
+        }
 
-
+    })
+    .then( console.log )
+    .catch(error => {
+        console.log('Error en la peticion');
+        console.log(error);
     });
